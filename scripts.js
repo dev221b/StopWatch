ac84bba4b931ddcadf4d5163252b1c9b4a46b99d
dev221b <92394193+dev221b@users.noreply.github.com>
2022-03-08 00:04:58 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -63,43 +63,4 @@ window.onload = function(){
       }
     }
 
-    // $(document).ready(function(){
-
-    //   $('#time').click(function(){
-    //     $(this).toggleClass('fa-times');
-    //     $('header').toggleClass('toggle');
-    //   });
     
-    //   $(window).on('scroll load',function(){
-    
-    //     $('#time').removeClass('fa-times');
-    //     $('header').removeClass('toggle');
-    
-    //     if($(window).scrollTop() > 0){
-    //       $('.top').show();
-    //     }else{
-    //       $('.top').hide();
-    //     }
-    
-    //   });
-    
-    //   // smooth scrolling 
-    
-    //   $('a[href*="#"]').on('click',function(e){
-    
-    //     e.preventDefault();
-    
-    //     $('html, body').animate({
-    
-    //       scrollTop : $($(this).attr('href')).offset().top,
-    
-    //     },
-    //       500, 
-    //       'linear'
-    //     );
-    
-    //   });
-    
-    // });
-    
-   
