47ffa2b5c41592578173d389de9b0c007171f39d
dev221b <92394193+dev221b@users.noreply.github.com>
2022-03-08 00:00:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -63,43 +63,43 @@ window.onload = function(){
       }
     }
 
-    $(document).ready(function(){
+    // $(document).ready(function(){
 
-      $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
-      });
+    //   $('#time').click(function(){
+    //     $(this).toggleClass('fa-times');
+    //     $('header').toggleClass('toggle');
+    //   });
     
-      $(window).on('scroll load',function(){
+    //   $(window).on('scroll load',function(){
     
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
+    //     $('#time').removeClass('fa-times');
+    //     $('header').removeClass('toggle');
     
-        if($(window).scrollTop() > 0){
-          $('.top').show();
-        }else{
-          $('.top').hide();
-        }
+    //     if($(window).scrollTop() > 0){
+    //       $('.top').show();
+    //     }else{
+    //       $('.top').hide();
+    //     }
     
-      });
+    //   });
     
-      // smooth scrolling 
+    //   // smooth scrolling 
     
-      $('a[href*="#"]').on('click',function(e){
+    //   $('a[href*="#"]').on('click',function(e){
     
-        e.preventDefault();
+    //     e.preventDefault();
     
-        $('html, body').animate({
+    //     $('html, body').animate({
     
-          scrollTop : $($(this).attr('href')).offset().top,
+    //       scrollTop : $($(this).attr('href')).offset().top,
     
-        },
-          500, 
-          'linear'
-        );
+    //     },
+    //       500, 
+    //       'linear'
+    //     );
     
-      });
+    //   });
     
-    });
+    // });
     
    
